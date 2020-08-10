4eb1b6ff14943564362e85467e9adca8de61cf72
sam <juckrit072@gmail.com>
2020-08-10 20:26:08 +0700

    can run

--- app/src/main/java/com/example/mentionview/MainActivity.kt
+++ app/src/main/java/com/example/mentionview/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.mentionview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mentionview.data.City
+import com.example.mentionview.suggestions.SuggestionsResult
+import com.example.mentionview.tokenization.QueryToken
+import com.example.mentionview.tokenization.interfaces.QueryTokenReceiver
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), QueryTokenReceiver {
+
+    private val BUCKET = "cities-memory"
+
+    private var cities: City.CityLoader? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.simple_mentions)
+
+        editor.displayTextCounter(false)
+        editor.setQueryTokenReceiver(this)
+        editor.setHint(resources.getString(R.string.type_city))
+        cities = City.CityLoader(resources)
+    }
+
+    override fun onQueryReceived(queryToken: QueryToken): List<String> {
+        val buckets =
+            listOf<String>("bucket")
+        val suggestions: List<City> = cities?.getSuggestions(queryToken)!!
+        val result = SuggestionsResult(queryToken, suggestions)
+        editor.onReceiveSuggestionsResult(
+            result,
+            "bucket"
+        )
+        return buckets
     }
 }
